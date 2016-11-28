8cabf427aafd4e1e262ca873964697eb6f86d4ba
githubashutoshsoni <m.ashutoshsoni@gmail.com>
2016-11-28 16:16:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -182,6 +182,10 @@ app.post('/create-user', function (req, res) {
    // JSON
    var username = req.body.username;
    var password = req.body.password;
+   if(!username.trim() || !password.trim()){
+     res.status(400).send('Username or password field blank.');   //Err if blank,tabs and space detected.
+  }
+  else {
    var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt);
    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
@@ -191,13 +195,18 @@ app.post('/create-user', function (req, res) {
           res.send('User successfully created: ' + username);
       }
    });
+  }
 });
 
 app.post('/login', function (req, res) {
    var username = req.body.username;
    var password = req.body.password;
+   if(!username.trim() || !password.trim()){
+     res.status(400).send('Username or password field blank.');   //Err if blank,tabs and space detected.
+  }
+    else{
    
-   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
+         pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
@@ -223,7 +232,9 @@ app.post('/login', function (req, res) {
               }
           }
       }
+        
    });
+    }
 });
 
 app.get('/check-login', function (req, res) {
